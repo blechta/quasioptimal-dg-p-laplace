e762a1a6897803c0f3a0a37358762e7329115a0a
Jan Blechta <blechta@karlin.mff.cuni.cz>
2023-07-28 13:33:16 +0200

    Improve Docker workflow

--- code/run_container.sh
+++ code/run_container.sh
@@ -1,2 +1,43 @@
 #!/bin/bash
-docker run --rm -ti -v $PWD:/home/firedrake/shared -v $PWD/.bash_history:/home/firedrake/.bash_history -v $PWD/.python_history:/home/firedrake/.python_history firedrakeproject/firedrake
+set -e
+
+# Select image
+IMAGE="fd:paper-smoothing-op"  # FIXME: currently a custom firedrake tag
+
+THISDIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+CONTAINER_MOUNT_POINT="/home/firedrake/shared"
+
+if [ -t 0 ] ; then
+    # Interactive
+    DOCKER_RUN="docker run --interactive --tty"
+else
+    # No input terminal
+    DOCKER_RUN="docker run"
+fi
+
+read -r -d '' DOCKER_RUN_CMD <<EOF || true
+${DOCKER_RUN}
+  --rm \
+  --volume=${THISDIR}:${CONTAINER_MOUNT_POINT} \
+  --env=DISPLAY=${DISPLAY} \
+  --volume=/tmp/.X11-unix:/tmp/.X11-unix \
+  --workdir=${CONTAINER_MOUNT_POINT} \
+  ${IMAGE} \
+  /bin/bash -c
+EOF
+
+read -r -d '' CONTAINER_CMD <<EOF || true
+set -e
+ln -s ${CONTAINER_MOUNT_POINT}/.{bash,python}_history ~/
+source ~/firedrake/bin/activate
+export OMP_NUM_THREADS=1
+rm -rf \${VIRTUAL_ENV}/.cache
+ln -s ${CONTAINER_MOUNT_POINT}/.cache \${VIRTUAL_ENV}/
+mkdir -p ${CONTAINER_MOUNT_POINT}/.cache/tsfc/
+#pip install --no-cache-dir pdbpp
+set -x
+"\$0" "\$@"
+EOF
+
+set -x
+${DOCKER_RUN_CMD} "${CONTAINER_CMD}" "$@"
