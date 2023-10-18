b7f5ba70a98c6b4c8e45850763d77d870e818ee9
Jan Blechta <blechta@karlin.mff.cuni.cz>
2023-10-18 15:17:49 +0200

    Drop files from Karlin cluster

---
+++ code/batch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH --job-name=paper-quasioptimal
+#SBATCH --output=paper-quasioptimal.log
+#
+#SBATCH -n 18
+#SBATCH --time=12:00:00
+#SBATCH -p express3
+#SBATCH -w r32
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=blechta@karlin.mff.cuni.cz
+
+cd /usr/work/blechta/dev/paper-smoothing-op/code
+udocker run -v $PWD:/mnt quasioptimal bash /mnt/script.sh
